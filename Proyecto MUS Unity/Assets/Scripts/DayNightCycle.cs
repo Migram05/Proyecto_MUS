1d7d3075ab6787153cc2b55fa1a324fce4174bc3
roiqd03 <91754567+roiqd03@users.noreply.github.com>
2024-05-18 13:20:14 +0200

    canción de desierto implementada en fmod y unity

--- Proyecto MUS Unity/Assets/Scripts/DayNightCycle.cs
+++ Proyecto MUS Unity/Assets/Scripts/DayNightCycle.cs
@@ -1,5 +1,7 @@
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
@@ -7,13 +9,23 @@ public class DayNightCycle : MonoBehaviour
     [SerializeField]
     float rotationSpeed = 1.0f;
 
+    [SerializeField]
+    private StudioEventEmitter _emitter;
+    
     private float timeSum = 0;
     public float currentTime = 0.0f;
+
+    [SerializeField]
+    private int dayHour = 10;
+    [SerializeField]
+    private int nightHour = 21;
+
 
     void Update()
     {
         gameObject.transform.Rotate(rotationSpeed * Time.deltaTime, 0f, 0f);
         timeSum += (rotationSpeed * Time.deltaTime);
         currentTime = ((timeSum / 360) * 24) % 24;
+        _emitter.SetParameter("Day", (currentTime > dayHour && currentTime < nightHour) ? 1 : 0);
     }
 }
