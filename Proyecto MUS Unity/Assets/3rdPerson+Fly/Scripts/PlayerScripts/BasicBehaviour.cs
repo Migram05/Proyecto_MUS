c66be96d611e36c35f10f712185cdd53b3a9793f
roiqd03 <91754567+roiqd03@users.noreply.github.com>
2024-05-19 12:48:39 +0200

    Cerrar aplicación con ESC

--- Proyecto MUS Unity/Assets/3rdPerson+Fly/Scripts/PlayerScripts/BasicBehaviour.cs
+++ Proyecto MUS Unity/Assets/3rdPerson+Fly/Scripts/PlayerScripts/BasicBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 // This class manages which player behaviour is active or overriding, and call its local functions.
 // Contains basic setup and common functions used by all the player behaviours.
@@ -90,7 +91,7 @@ public class BasicBehaviour : MonoBehaviour
 
 		if(Input.GetKeyDown(KeyCode.Escape))
 		{
-			Cursor.lockState = CursorLockMode.None;
+			Application.Quit();
 		}
 	}
 
